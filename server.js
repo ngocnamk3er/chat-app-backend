f04fe132ce4e8e515e87c715c7c484eb9b463351
ngocnam <ngocnamk3er@gmail.com>
2023-06-07 23:52:30 +0700

    add cors from vercel front end

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
     pingTimeout: 60000,
     cors: {
-        origin: "http://localhost:3000",
+        origin: "https://front-end-chat-app-opal.vercel.app/",
         // credentials: true,
     },
 });
